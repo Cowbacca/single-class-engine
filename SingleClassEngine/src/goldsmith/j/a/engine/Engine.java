c7953d52654d872576ca27e6a5588a83c25b9adf
Angus Goldsmith <durins_beard@hotmail.com>
2015-11-01 22:26:14 +0000

    Refactored to use lambdas.

--- SingleClassEngine/src/goldsmith/j/a/engine/Engine.java
+++ SingleClassEngine/src/goldsmith/j/a/engine/Engine.java
@@ -6,28 +6,17 @@ public class Engine {
     private Row row;
 
     public void setup() {
-        Inputs inputs = new Inputs(11, 2);
-
-        Condition condition = new Condition() {
-
-            public boolean passesCondition(Inputs inputs) {
-                if (inputs.getAge() > 10) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        };
-
-        ScoreLambda scoreLambda = new ScoreLambda() {
-
-            public int run(Inputs inputs) {
-                return inputs.getTimeOff() * 2;
-            }
-
-        };
-
-        row = Row.newBuilder(inputs, condition).scoreLambda(scoreLambda).build();
+        row = Row.newBuilder(
+                new Inputs(11, 2),
+                conditionInputs -> {
+                    if (conditionInputs.getAge() > 10) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                })
+                .scoreLambda(scoreInputs -> scoreInputs.getTimeOff() * 2)
+                .build();
     }
 
     public void run() {
